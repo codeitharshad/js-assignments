fd49c1aded1973a730f650e8df828a205f418ba9
Harshad Gangurde <143880663+codeitharshad@users.noreply.github.com>
2024-08-06 12:09:42 +0530

    Reviewed and resolved  all mistakes.

--- js_assignment/eliminateDuplicateElement.js
+++ js_assignment/eliminateDuplicateElement.js
@@ -1,42 +1,12 @@
-function eliminateDuplicateElement(arr){
+function eliminateDuplicateElement(arr) {
+  let result = [];
 
-    // Creating array with unique values 
-    let result = [];
-    const obj = {};
+  arr.forEach((element) => {
+    if (!result.includes(element)) result.push(element);
+  });
 
-    arr.forEach(element => {
-        if(!result.includes(element))
-        result.push(element);
-    });
-
-    // Creating object from array of unique values with the value undefined 
-    result.forEach(element => {
-        obj[element] = undefined;
-    });     
-
-    // Counting the frequency of each key in array and setting it as value to key
-    for(key in obj){
-        let count = 0;
-        arr.forEach(element => {
-            if(parseInt(key) === element){
-                count++;
-            }
-        });
-        obj[key] = count;
-    }
-
-    // FInding key which have highest value
-    maxKey = null;
-    maxValue = -1;
-
-    for(const key in obj){
-
-        if(obj[key] > maxValue){
-            maxKey = obj[key];
-        }
-    }
-    return maxKey
+  return result;
 }
 
-let arr = [1,1,1,2,2,2,1,7,7,7,7,7,7,7,7,7,7,8,8,8,8,8,8,8,8,8,8,8,8,8]
-console.log(eliminateDuplicateElement(arr))
+let arr = [11, 2, 3, 4, 3, 5, 6, 5, 4, 7];
+console.log(eliminateDuplicateElement(arr));
---
+++ js_assignment/mostRepeatedElement.js
@@ -0,0 +1,42 @@
+function mostFrequentElement(arr){
+
+    // Creating array with unique values 
+    let result = [];
+    const obj = {};
+
+    arr.forEach(element => {
+        if(!result.includes(element))
+        result.push(element);
+    });
+
+    // Creating object from array of unique values with the value undefined 
+    result.forEach(element => {
+        obj[element] = undefined;
+    });     
+
+    // Counting the frequency of each key in array and setting it as value to key
+    for(key in obj){
+        let count = 0;
+        arr.forEach(element => {
+            if(parseInt(key) === element){
+                count++;
+            }
+        });
+        obj[key] = count;
+    }
+
+    // FInding key which have highest value
+    maxKey = null;
+    maxValue = -1;
+
+    for(const key in obj){
+
+        if(obj[key] > maxValue){
+            maxKey = obj[key];
+        }
+    }
+    return maxKey
+}
+
+let arr = [1,1,2,2,3,3,3]
+console.log(mostFrequentElement(arr))
